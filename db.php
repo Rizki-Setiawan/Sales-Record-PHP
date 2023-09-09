81d3378f2cb50842c6b6e91b4f28e22b31a6bb96
root <root@kise.kise.net>
2023-09-09 13:29:56 +0700

    update db.php

--- db.php
+++ db.php
@@ -1,9 +1,7 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","db_gerewek");
+$con = mysqli_connect("localhost", "root", "", "db_gerewek");
 
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: ".mysqli_connect_error();
-  }
-?>
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
